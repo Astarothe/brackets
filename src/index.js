58e6c27488697d58888a7fa82f63e434c6aeb0b6
Mishel Savich <minatosavich@gmail.com>
2020-10-21 22:48:53 +0300

    add task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let massive = bracketsConfig.map(element => element.join(''));
+    let strLong = str.length;
+    while(strLong > 0){
+        massive.forEach(function(elements){
+            str = str.replace(elements,'');
+        });
+        if(str.length === strLong){
+            return false;
+        }else if(str.length === 0){
+           return true;
+       }else{
+           strLong = str.length;
+       }           
+    }
 }
