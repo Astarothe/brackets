9b0de5e13af01570f3183da336d59f14b432a6b1
Astaroth <minatosavich@gmail.com>
2021-02-21 20:42:09 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-    let massive = bracketsConfig.map(element => element.join(''));
-    let strLong = str.length;
-    while(strLong > 0){
-        massive.forEach(function(elements){
-            str = str.replace(elements,'');
-        });
-        if(str.length === strLong){
-            return false;
-        }else if(str.length === 0){
-           return true;
-       }else{
-           strLong = str.length;
-       }           
+  let stack = [];
+  for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+      if(str[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0]){
+        stack.pop();
+      } else if(str[i] === bracketsConfig[j][0]){
+        stack.push(str[i]);
+      }
     }
+  };
+  if(stack.length !== 0 || str.length % 2 !== 0){
+    return false;
+  }
+  return true;
 }
